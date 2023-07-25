07268a06a0a1c0223a7b668b56d45527a7ca57eb
dragox134 <legi1810@gmail.com>
2023-07-25 20:33:23 +0200

    login counter

--- Academy/lvl2/index.php
+++ Academy/lvl2/index.php
@@ -1,8 +1,24 @@
 <?php
     if (isset($_GET["submit"])){
-        file_put_contents("log.json", $_GET["name"] . "<br>\n", FILE_APPEND);
-        echo "Name: " . $_GET["name"] . "<br>";
+        $name = $_GET["name"];
+        $logins = [];
     }
+
+    if (file_exists("log.json")){
+        $logins = json_decode(file_get_contents("log.json"), true);
+    }
+
+    if(isset($logins[$name])){
+        $logins[$name]++;
+    }
+    else{
+        $logins[$name] = 1;
+    }
+
+    file_put_contents("log.json", json_encode($logins, JSON_PRETTY_PRINT));
+
+
+    echo "Name: " . $name . " logins:" . $logins[$name] . "<br>";
 ?>
 
 <form method="GET">
